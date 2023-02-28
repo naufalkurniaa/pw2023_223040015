7b7bb905e4268cabf89e71d7926986971a4bd688
naufalkurniaa <knightemperor08@gmail.com>
2023-02-28 23:58:03 +0700

    mengerjakan tugas2

--- tugas/tugas2/2d.php
+++ tugas/tugas2/2d.php
@@ -5,34 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas2c</title>
-    <style>
-        .kotakhitam {
-            background-color: black;
-            width: 50px;
-            height: 50px;
-        }
-
-        .kotakputih {
-            background-color: white;
-            width: 50px;
-            height: 50px;
-        }
-    </style>
 </head>
 <body>
-<?php 
 
-for ($i=1;$i<=5;$i++) {
+
+<?php
+
+echo "<table border='1'>";
+for ($i = 1; $i <= 5; $i++) {
     echo "<tr>";
-    for ($j=1;$j<=5;$j++) {
-        if (($i+$j) % 2 == 0) {
-            echo "<td class="kotakhitam"></td>;
-        } 
+    for ($j = 1; $j <= 5; $j++) {
+        if (($i + $j) % 2 == 0) {
+            echo "<td style='background-color:black;width:50px;height:50px;'></td>";
+        } else {
+            echo "<td style='background-color:white;width:50px;height:50px;'></td>";
+        }
     }
+    echo "</tr>";
 }
+echo "</table>";
 
-?>    
-
+?>
 
 </body>
 </html>
